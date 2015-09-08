f8315450f9752ff508c07402eab539f9236bb745
Firdaus Zahari <fake@fsylum.net>
2015-09-08 15:42:01 +0800

    Update provisioning to include nginx and php-fpm support

--- Vagrantfile
+++ Vagrantfile
@@ -41,15 +41,17 @@ Vagrant.configure(2) do |config|
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
-  config.vm.synced_folder "www", "/var/www/"
+  config.vm.synced_folder "www/", "/srv/www/", :owner => "www-data", :mount_options => [ "dmode=775", "fmode=774" ]
+  config.vm.synced_folder "logs/", "/srv/logs", :owner => "www-data"
+  config.vm.synced_folder "config/", "/srv/config"
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
   # Example for VirtualBox:
   config.vm.provider "virtualbox" do |vb|
-    vb.gui = false
+    vb.gui  = false
     vb.name = File.basename(Dir.pwd); #taken from VVV
-    vb.customize ["modifyvm", :id, "--memory", 1024]
+    vb.customize ["modifyvm", :id, "--memory", 512]
     vb.customize ["modifyvm", :id, "--cpus", 1]
     vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
     vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
@@ -76,5 +78,5 @@ Vagrant.configure(2) do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  config.vm.provision "shell", path: "provision.sh"
+  config.vm.provision "shell", path: "provision/default.sh"
 end
--- provision/default.sh
+++ provision/default.sh
@@ -3,3 +3,125 @@
 # Default provisioning file that will be used by Vagrant whenever
 # `vagrant up`, ` vagrant provision` or `vagrant reload` commands are
 # used.
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Set start time
+time_start="$(date +%s)"
+
+# Add additional sources for packages
+echo "Updating package sources..."
+ln -sf /srv/config/apt-sources.list /etc/apt/sources.list.d/apt-sources.list
+
+# Add nginx signing key
+wget --quiet http://nginx.org/keys/nginx_signing.key -O- | apt-key add -
+
+# List of all required packages
+apt_packages=(
+    curl
+    dos2unix
+    gettext
+    git
+    imagemagick
+    #mysql-server
+    nginx
+    #nodejs
+    php5-cli
+    php5-common
+    php5-curl
+    php5-dev
+    php5-fpm
+    php5-gd
+    php5-imagick
+    php5-imap
+    php5-mcrypt
+    php5-mysqlnd
+    php5-xmlrpc
+    php-pear
+    #phpmyadmin
+    unzip
+    zip
+)
+
+# Update the list
+echo "Updating package list..."
+apt-get update --assume-yes
+
+# Loop through all the packages to filter only the one that yet to be installed
+for i in "${!apt_packages[@]}"; do
+    package_version="$(dpkg -s ${apt_packages[$i]} 2>&1 | grep 'Version:' | cut -d " " -f 2)"
+    if [[ -n "${package_version}" ]]; then
+        echo " *" ${apt_packages[$i]} [already installed]
+        unset apt_packages[$i]
+    else
+        echo " *" ${apt_packages[${i}]} [not installed]
+    fi
+done
+
+# Installe the packages
+if [[ ${#apt_packages[@]} = 0 ]]; then
+    echo "No package to install"
+else
+    echo "Installing required packages..."
+    apt-get install --assume-yes ${apt_packages[@]}
+fi
+
+# Post installation cleanup
+echo "Cleaning up..."
+apt-get clean
+
+# nginx
+echo "Modifying nginx configuration..."
+cp /srv/config/nginx/nginx.conf /etc/nginx/nginx.conf
+cp /srv/config/nginx/default.conf /etc/nginx/conf.d/default.conf
+
+# PHP-FPM
+echo "Modifying php5-fpm configuration..."
+cp /srv/config/php5-fpm/www.conf /etc/php5/fpm/pool.d/www.conf
+cp /srv/config/php5-fpm/php-custom.ini /etc/php5/fpm/conf.d/php-custom.ini
+
+# Restart all the services
+echo "Restarting services..."
+service php5-fpm restart
+service nginx restart
+
+# Add vagrant user to the www-data group
+usermod -a -G www-data vagrant
+
+# Calculate time taken and inform the user
+time_end="$(date +%s)"
+echo "Provisioning complete in "$(expr $time_end - $time_start)" seconds"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# npm install -g npm
+# npm install -g npm-check-updates
+
+# MySQL
+#
+# Use debconf-set-selections to specify the default password for the root MySQL
+# account. This runs on every provision, even if MySQL has been installed. If
+# MySQL is already installed, it will not affect anything.
+#echo mysql-server mysql-server/root_password password root | debconf-set-selections
+#echo mysql-server mysql-server/root_password_again password root | debconf-set-selections
+
+# if [[ ! -n "$(composer --version --no-ansi | grep 'Composer version')" ]]; then
+#         echo "Installing Composer..."
+#         curl -sS https://getcomposer.org/installer | php
+#         chmod +x composer.phar
+#         mv composer.phar /usr/local/bin/composer
+#     fi
+
